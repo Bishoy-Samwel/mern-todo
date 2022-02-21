2a98a4eeee1236990b02e0e3949380686b96bf44
Bishoy <bishoy.samwuel@gmail.com>
2022-02-21 13:49:09 +0200

    - Require body-parser, mongoose, routes - Connect to the DB - handle CORS related issues - User bodyParser, routes

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,37 @@
 const express = require('express');
-const router = express.Router();
-const Todo = require('../models/todo');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+require('dotenv').config();
 
-router.get('/todos', (req, res, next) => {
-  // This will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
-    .then((data) => res.json(data))
-    .catch(next);
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Connect to the database
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log(`Database connected successfully`))
+  .catch((err) => console.log(err));
+
+// Since mongoose's Promise is deprecated, we override it with Node's Promise
+mongoose.Promise = global.Promise;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-router.post('/todos', (req, res, next) => {
-  if (req.body.action) {
-    Todo.create(req.body)
-      .then((data) => res.json(data))
-      .catch(next);
-  } else {
-    res.json({
-      error: 'The input field is empty',
-    });
-  }
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 });
 
-router.delete('/todos/:id', (req, res, next) => {
-  Todo.findOneAndDelete({ _id: req.params.id })
-    .then((data) => res.json(data))
-    .catch(next);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-module.exports = router;
